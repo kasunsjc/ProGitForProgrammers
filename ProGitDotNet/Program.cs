18e4ec13ea6cfacfb52241c4845f4d85bc3bd236
Kasun Rajapakse <kasunsjc@hotmail.com>
2021-07-12 22:18:07 +0530

    first commit - from commandline

---
+++ ProGitDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProGitDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
