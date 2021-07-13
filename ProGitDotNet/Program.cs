60ea95a95e2a6ad8672aca7ef35b05417555f210
Kasun Rajapakse <kasunsjc@hotmail.com>
2021-07-13 23:32:07 +0530

    change hello world line

--- ProGitDotNet/Program.cs
+++ ProGitDotNet/Program.cs
@@ -6,7 +6,7 @@ namespace ProGitDotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I just added this line");
         }
     }
 }
